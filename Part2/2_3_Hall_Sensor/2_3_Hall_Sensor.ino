e35deb85f7b90125af8cff4512d7792c8ae2eef8
CJ <cjf8140@gmail.com>
2026-03-30 14:14:50 +0900

    홀센서 스테이트 머신으로 바꿈

--- Part2/2_3_Hall_Sensor/2_3_Hall_Sensor.ino
+++ Part2/2_3_Hall_Sensor/2_3_Hall_Sensor.ino
@@ -1,13 +1,31 @@
-void setup() {
+int state = 0;  //0이면 3, 1이면 5켜기
+
+void setup()
+{
   pinMode(2, INPUT_PULLUP);
   pinMode(3, OUTPUT);
   pinMode(5, OUTPUT);
 }
 
-void loop() {
-  if (digitalRead(2) == LOW) { // 자석 감지하면 LOW신호(Active LOW)
-    digitalWrite(3,HIGH);
-    digitalWrite(5,LOW); 
+void loop()
+{
+  if (digitalRead(2) == LOW) {  // 자석 감지하면 LOW신호(Active LOW)
+    // v 대체 가능 v
+    if(state == 0) {
+      state = 1;
+    }
+    else {
+      state = 0;
+    }
+    // ^ 대체 코드 ^
+    // state = !state;
+
+    delay(500);
+  }
+
+  if (state == 0) {  
+    digitalWrite(3, HIGH);
+    digitalWrite(5, LOW);
   }
   else {
     digitalWrite(3, LOW);
