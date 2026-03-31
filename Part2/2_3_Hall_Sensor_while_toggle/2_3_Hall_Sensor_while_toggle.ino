f6427709c9e9c55772f0cf6841ca78472a388437
CJ <cjf8140@gmail.com>
2026-03-31 15:27:37 +0900

    홀센서 와일로 토글버전

--- Part2/2_3_Hall_Sensor_while_toggle/2_3_Hall_Sensor_while_toggle.ino
+++ Part2/2_3_Hall_Sensor_while_toggle/2_3_Hall_Sensor_while_toggle.ino
@@ -7,21 +7,22 @@ void setup()
   pinMode(5, OUTPUT);
 }
 
-void loop() 
+void loop()
+{
   digitalWrite(3, HIGH);
   digitalWrite(5, LOW);
   while(digitalRead(2) == LOW) {
     delay(100);
   }
-  while(digitalRead(2) == HIGH) {
+  while(digitalRead(2) == HIGH) {     // 감지 안됐을경우 루프
     delay(100);
-  }
+  }                                   // 감지 되면 탈출하며 다음 코드로
 
   digitalWrite(3, LOW);
   digitalWrite(5, HIGH);
-  while(digitalRead(2) == LOW) {
+  while(digitalRead(2) == LOW) {      // 대고 있을 경우 정지
     delay(100);
-  }
+  }                                   // 때면 이제 감지 될때까지 루프
   while(digitalRead(2) == HIGH) {
     delay(100);
   }
