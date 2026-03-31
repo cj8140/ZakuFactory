c00ac363b76e3d48b71146e2bf27f01e2d661182
SY Park <kyojis@empal.com>
2026-03-31 17:54:57 +0900

    Update 2_8_servo_with_button_joyliike.ino

--- Part2/2_8_servo_with_button_joyliike/2_8_servo_with_button_joyliike.ino
+++ Part2/2_8_servo_with_button_joyliike/2_8_servo_with_button_joyliike.ino
@@ -2,12 +2,12 @@
 
 Servo my_servo;
 
-int angle_servo = 45;
+int angle_servo = 90;
 
 void setup()
 {
   my_servo.attach(9);  //핀 연결
-  my_servo.write(45);
+  my_servo.write(angle_servo);
 
   pinMode(2, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
@@ -17,16 +17,16 @@ void loop()
 {
   if (digitalRead(2) == LOW) {
     angle_servo++;
-    if(angle_servo > 180) {
-      angle_servo = 180;
+    if(angle_servo > 175) {
+      angle_servo = 175;
     }
     my_servo.write(angle_servo);
     delay(10);
   }
   if (digitalRead(4) == LOW) {
     angle_servo--;
-    if(angle_servo < 0) {
-      angle_servo = 0;
+    if(angle_servo < 5) {
+      angle_servo = 5;
     }
     my_servo.write(angle_servo);
     delay(10);
