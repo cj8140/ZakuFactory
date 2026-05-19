e4ab8549f1f4fe516ee0ed718b476ed271fe6a42
SY Park <kyojis@empal.com>
2026-05-19 17:58:22 +0900

    semi colon

--- Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
+++ Part2/2_9_servo_with_potentiometer/2_9_servo_with_potentiometer.ino
@@ -4,20 +4,19 @@ Servo my_servo;
 void setup()
 {
   Serial.begin(9600);
-
   my_servo.attach(9);  //핀 연결
-  my_servo.write(90); // //가변저항핀모드 인풋 삭제
+  my_servo.write(90); //
 }
 
 void loop()
 {
-  int value_pot = analogRead(A1)
+  int value_pot = analogRead(A1);
   int angle_servo = map(value_pot, 0, 1023, 0, 180);
   my_servo.write(angle_servo);
 
-  Serial.print("Pot: ")
+  Serial.print("Pot: ");
   Serial.print(value_pot);
-  Serial.print(", Angle: ")
+  Serial.print(", Angle: ");
   Serial.println(angle_servo);
 
   // v 분리된것을 기본으로하고 한 줄로도 할 수 있다는 코드 설명에서 v
